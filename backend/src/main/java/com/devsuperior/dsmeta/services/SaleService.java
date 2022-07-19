041dd2887c8d0650d94770ecb4aca44529c1f4c1
MartinsJoaoPedro <jpmartinsfilho1@gmail.com>
2022-07-19 18:35:02 -0300

    Date select

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,10 @@
 package com.devsuperior.dsmeta.services;
 
-import java.util.List;
+import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -13,8 +15,23 @@ public class SaleService {
 
 	@Autowired
 	private SaleRepository repository;
-	
-	public List<Sale> findSales() {
-		return repository.findAll();
+
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		LocalDate min;
+		LocalDate max;
+		LocalDate agora = LocalDate.now();
+		if (minDate.equals("")) {
+			min = (LocalDate.of(agora.getYear(), agora.getMonth(), agora.getDayOfMonth())).minusYears(1);
+					
+		}else {
+			min = LocalDate.parse(minDate);
+		}
+		if (minDate.equals("")) {
+			max = (LocalDate.of(agora.getYear(), agora.getMonth(), agora.getDayOfMonth()));
+		}else {
+			max = LocalDate.parse(maxDate);
+		}
+
+		return repository.findSales(min, max, pageable);
 	}
 }
